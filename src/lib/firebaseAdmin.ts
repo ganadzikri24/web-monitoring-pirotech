34444b36e7bdf3a32148488c57f01c85600b29e1
Muhamad Ganabitz Dzikri <ganadzikri@gmail.com>
2026-09-11 17:58:14 +0700

    Fix database

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -10,7 +10,9 @@ function getAdminApp(): App {
     credential: cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"|"$/g, '')
+        : undefined,
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
